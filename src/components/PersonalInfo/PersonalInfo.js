dd0fa244e94f64322f8d9bfee6014620c574b2e7
k-m-rahman <k_m_rahman23@hotmail.com>
2022-09-29 20:05:21 +0600

    edited personal info

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -8,26 +8,26 @@ const PersonalInfo = () => {
 
             {/* Name , picture and location */}
             <div className='flex gap-4 items-center justify-center'>
-                <img className="rounded-full w-1/5 lg:w-1/5 md:w-[12%]" src="https://www.celebrityborn.com/admin/assets/images/people/ananta_jalil_208.jpg" alt='ananta jalil'></img>
+                <img className="rounded-full w-1/5 lg:w-1/5 md:w-[12%]" src="https://phero-web.nyc3.cdn.digitaloceanspaces.com/website-prod-images/public/files/1660118580754.png" alt=''></img>
                 <div className=''>
-                    <h3 className='font-semibold text-md'>Ananta Jalil</h3>
+                    <h3 className='font-semibold text-md'>Khandakar Mahmudur Rahman</h3>
                     <p className='text-sm text-slate-600'> <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> Dhaka,Bangladesh</p>
                 </div>
             </div>
 
             {/* weight , height and age */}
 
-            <div className='flex gap-5 bg-blue-100 p-3 justify-center rounded-xl mt-5 shadow-lg w-2/3 lg:w-full mx-auto text-center'>
+            <div className='flex gap-5 bg-blue-100 p-3 justify-center items-center rounded-xl mt-5 shadow-lg w-2/3 lg:w-full mx-auto text-center'>
                 <div>
-                    <h2 className='font-bold'>Freelancer</h2>
+                    <h2 className='font-bold'>Web Developer</h2>
                     <h3 className='text-xs font-medium text-slate-700'>Occupation</h3>
                 </div>
                 <div>
-                    <h2 className='font-bold'>4.00</h2>
+                    <h2 className='font-bold'>3.00</h2>
                     <h3 className='text-xs font-medium text-slate-700'>CG</h3>
                 </div>
                 <div>
-                    <h2 className='font-bold'>35<span className='text-sm text-slate-600'>yrs</span></h2>
+                    <h2 className='font-bold'>28<span className='text-sm text-slate-600'>yrs</span></h2>
                     <h3 className='text-xs font-medium text-slate-700'>Age</h3>
                 </div>
             </div>
